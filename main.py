eb621ade8a1089fb85e33652f09452f548043323
Nitin Shankar Madhu <nitinshankarmadhu@gmail.com>
2026-03-26 19:46:01 -0400

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world() -> dict[str, str]:
+    return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("main:app", reload=True)
